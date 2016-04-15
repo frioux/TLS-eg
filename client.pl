4dea564ec88c583e368e1f5a7341580022b56d5b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2016-04-14 21:13:34 -0700

    client2

--- client.pl
+++ client.pl
@@ -4,16 +4,20 @@ use strict;
 use warnings;
 
 use IO::Async::Loop;
+use IO::Socket::SSL;
 use Net::Async::HTTP;
 
 my $loop = IO::Async::Loop->new;
 
 $loop->add(
    my $ua = Net::Async::HTTP->new(
-      SSL_key_file => 'web.key',
-      SSL_cert_file => 'web.pem',
+      SSL_key_file => 'client.key',
+      SSL_cert_file => 'client.pem',
       SSL_ca_file => 'rootCA.pem',
+      SSL_verify_mode => SSL_VERIFY_NONE,
    )
 );
 
-print $ua->GET('https://localhost:8000/serial')->get->decoded_content;
+my $res = $ua->GET('https://localhost:8000/serial')->get;
+
+print $res->decoded_content;
